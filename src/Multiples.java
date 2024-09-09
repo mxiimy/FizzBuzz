212bdd318a8cca2e175ab38513fc8c688fda1865
mxiimy <momelody33@gmail.com>
2024-09-09 10:37:00 -0400

    Counts multiples of 3 or 5 up to 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int a = 1; a < 1000; a++){
+            boolean divisibleBy3 = a % 3 == 0;
+            boolean divisibleBy5 = a % 5 == 0;
+            if (divisibleBy5 || divisibleBy3){
+                num++;
+            }
+        }
+        System.out.println(num);
+    }
 }

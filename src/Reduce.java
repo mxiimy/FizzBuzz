a51c430b5598166f0f896e5b005db87976933bd0
mxiimy <momelody33@gmail.com>
2024-09-09 10:37:26 -0400

    Counts steps needed to reach 0 if n = 10

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        int n = 10;
+        while (n > 0 ){
+            if (n % 2 == 0) {
+                n /= 2;
+                steps++;
+            } else {
+                n -= 1;
+                steps++;
+            }
+        }
+        System.out.println(steps);
+    }
 }
